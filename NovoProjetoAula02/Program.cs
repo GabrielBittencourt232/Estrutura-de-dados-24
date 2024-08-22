b6a7b94c50c90ae3ebf68352ab427237918c2f96
Gabriel Antônio Bittencourt <427326@unoesc.lan>
2024-08-21 21:51:40 -0300

    Aula Classe, Objeto E num

---
+++ NovoProjetoAula02/Program.cs
@@ -0,0 +1,14 @@
+﻿
+using NovoProjetoAula02;
+
+Console.WriteLine("Hello, World!");
+
+// new TipoEnumerador() -> Invoca o método construtor do objeto
+// Toda classe tem o seu construtor padrão implicito sem argumentos/params
+// É possível sobrescrever este método especificando argumentos 
+// Todavia, se o fizermos, perdemos o original implícito e 
+// precisaremos defini-lo explicitamente
+TipoEnumerador tipoEnum = new TipoEnumerador();
+
+TipoEnumerador.Language enumEnglish = tipoEnum.GetLanguageEnum("inglês");
+Console.WriteLine($"O enum de english é {enumEnglish}");

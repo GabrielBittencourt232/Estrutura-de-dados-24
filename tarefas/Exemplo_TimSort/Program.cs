b0cc97173a5cd095062d77deeaa6b6c3489968c1
Gabriel <gabrielbittencourt1036@gmail.com>
2024-10-22 00:08:41 -0300

    TrabalhoTimSort

---
+++ tarefas/Exemplo_TimSort/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int[] array = { 5, 2, 9, 1, 5, 6 };
+
+        // Array.Sort() usa TimSort internamente
+        Array.Sort(array);
+
+        Console.WriteLine("Array Ordenado:");
+        foreach (int num in array)
+        {
+            Console.Write(num + " ");
+        }
+    }
+}

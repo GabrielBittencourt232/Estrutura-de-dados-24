c94e449f05f7e37a11a3b0725e7642d9c05f5978
Gabriel Antônio Bittencourt <427326@unoesc.lan>
2024-10-23 20:50:37 -0300

    atividade

--- tarefas/Exemplo_TimSort/Program.cs
+++ tarefas/Exemplo_TimSort/Program.cs
@@ -1,18 +1,34 @@
 ﻿using System;
 
-class Program
+class Compras
 {
     static void Main()
     {
-        int[] array = { 5, 2, 9, 1, 5, 6 };
+        string[] comprasArray = { "Frango", "Bife", "Maçã", "Banana", "Peixe", "Pera", "Tomate", "Laranja" };
 
-        // Array.Sort() usa TimSort internamente
-        Array.Sort(array);
+        // Simulando um Merge Sort, separando a array em 2 subarrays de tamanho = 4
+        string[] subArray1 = new string[4];
+        string[] subArray2 = new string[4];
 
-        Console.WriteLine("Array Ordenado:");
-        foreach (int num in array)
-        {
-            Console.Write(num + " ");
-        }
+        Array.Copy(comprasArray, 0, subArray1, 0, 4);
+        Array.Copy(comprasArray, 4, subArray2, 0, 4);
+
+        // Ordena as subarrays usando um Insertion Sort
+        Array.Sort(subArray1);
+        Array.Sort(subArray2);
+
+        // Junta as duas subarrays
+        string[] novaArray = new string[subArray1.Length + subArray2.Length];
+        Array.Copy(subArray1, 0, novaArray, 0, subArray2.Length);
+        Array.Copy(subArray2, 0, novaArray, subArray1.Length, subArray2.Length);
+
+        // Ordena a array que foi juntada
+        Array.Sort(novaArray);
+
+        // Resultado
+        Console.WriteLine("Itens do Mercado: {0}", string.Join(", ", comprasArray));
+        Console.WriteLine("1ª Array ordenada: {0}", string.Join(", ", subArray1));
+        Console.WriteLine("2º Array ordenada: {0}", string.Join(", ", subArray2));
+        Console.WriteLine("Array combinada e ordenada: {0}", string.Join(", ", novaArray));
     }
 }
